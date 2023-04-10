6dc81604312b03665136ffbb0a431f9ede7cc47e
yxiuhli <luctr1509@gmail.com>
2023-04-10 18:12:14 +0700

    edited setting code

--- screens/app/components/SettingPaneComponent.tsx
+++ screens/app/components/SettingPaneComponent.tsx
@@ -141,7 +141,7 @@ const AccountSettings: IComponent<AccountSettingsProps> = (props) => {
                   <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                     <button
                       type="button"
-                      className="w-full inline-flex justify-center rounded-md border border-blue-300 shadow-sm px-4 py-2 bg-red-500 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+                      className="w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-gray-300 text-base font-medium text-white hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:ml-3 sm:w-auto sm:text-sm"
                       onClick={() => handleClosePreview()}
                     >
                       Close
@@ -149,7 +149,7 @@ const AccountSettings: IComponent<AccountSettingsProps> = (props) => {
                     <button
                       type="submit"
                       onClick={handleImageSave}
-                      className="mt-3 w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                      className="mt-3 w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-700 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
                     >
                       Save
                     </button>
