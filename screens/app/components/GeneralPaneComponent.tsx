bc643f0aba25d57c7ff2cb3e63260250c51b27b0
paradox2604 <66421827+paradox2604@users.noreply.github.com>
2023-04-13 16:14:38 +0700

    Update GeneralPaneComponent.tsx

--- screens/app/components/GeneralPaneComponent.tsx
+++ screens/app/components/GeneralPaneComponent.tsx
@@ -1,4 +1,5 @@
 import {
+  Button,
   Tab,
   TabPanel,
   Tabs,
@@ -13,15 +14,258 @@ export const GeneralPaneComponent: IComponent = () => {
     {
       label: "Janitors",
       value: "janitors",
-      children: `It really matters and then like it really doesn't matter.
-          What matters is the people who are sparked by it. And the people 
-          who are like offended by it, it doesn't matter.`,
+      children: 
+        <table className="w-full text-sm text-center text-gray-500 dark:text-gray-400">
+          <thead className=" text-gray-900 border-8 bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                ID
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Tên 
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Giới tính
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Ngày sinh
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Khu vực
+              </th>
+              <th scope="col" className="px-6 py-3">
+                SĐT
+              </th>
+            </tr>
+          </thead>
+          <tbody className=" font-medium text-gray-900 whitespace-nowrap dark:text-white ">
+            <tr className="bg-white dark:bg-gray-800 border-8" >
+              <td className="px-6 py-2">1</td>
+              <td className="px-6 py-2">John</td>
+              <td className="px-6 py-2">Nam</td>
+              <td className="px-6 py-2">19/2/1973</td>
+              <td className="px-6 py-2">D.10</td>
+              <td className="px-6 py-2">0123456789</td>
+            </tr>
+            <tr className="bg-white dark:bg-gray-800 border-8" >
+              <td className="px-6 py-2">2</td>
+              <td className="px-6 py-2">John</td>
+              <td className="px-6 py-2">Nam</td>
+              <td className="px-6 py-2">19/2/1973</td>
+              <td className="px-6 py-2">D.10</td>
+              <td className="px-6 py-2">0123456789</td>
+            </tr>
+            <tr className="bg-white dark:bg-gray-800 border-8" >
+              <td className="px-6 py-2">3</td>
+              <td className="px-6 py-2">John</td>
+              <td className="px-6 py-2">Nam</td>
+              <td className="px-6 py-2">19/2/1973</td>
+              <td className="px-6 py-2">D.10</td>
+              <td className="px-6 py-2">0123456789</td>
+            </tr>
+            <tr className="bg-white dark:bg-gray-800 border-8" >
+              <td className="px-6 py-2">4</td>
+              <td className="px-6 py-2">John</td>
+              <td className="px-6 py-2">Nam</td>
+              <td className="px-6 py-2">19/2/1973</td>
+              <td className="px-6 py-2">D.10</td>
+              <td className="px-6 py-2">0123456789</td>
+            </tr>
+            <tr className="bg-white dark:bg-gray-800 border-8" >
+              <td className="px-6 py-2">5</td>
+              <td className="px-6 py-2">John</td>
+              <td className="px-6 py-2">Nam</td>
+              <td className="px-6 py-2">19/2/1973</td>
+              <td className="px-6 py-2">D.10</td>
+              <td className="px-6 py-2">0123456789</td>
+            </tr>
+            <tr className="bg-white dark:bg-gray-800 border-8" >
+              <td className="px-6 py-2">6</td>
+              <td className="px-6 py-2">John</td>
+              <td className="px-6 py-2">Nam</td>
+              <td className="px-6 py-2">19/2/1973</td>
+              <td className="px-6 py-2">D.10</td>
+              <td className="px-6 py-2">0123456789</td>
+            </tr>
+          </tbody>
+        </table>
     },
     {
       label: "Collectors",
       value: "collectors",
-      children: `Because it's about motivating the doers. Because I'm here
-          to follow my dreams and inspire other people to follow their dreams, too.`,
+      children: 
+        <table className="w-full text-sm text-center text-gray-500 dark:text-gray-400">
+          <thead className=" text-gray-900 border-8 bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                ID
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Tên 
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Giới tính
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Ngày sinh
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Khu vực
+              </th>
+              <th scope="col" className="px-6 py-3">
+                SĐT
+              </th>
+            </tr>
+          </thead>
+          <tbody className=" font-medium text-gray-900 whitespace-nowrap dark:text-white ">
+            <tr className="bg-white dark:bg-gray-800 border-8" >
+              <td className="px-6 py-2">1</td>
+              <td className="px-6 py-2">John</td>
+              <td className="px-6 py-2">Nam</td>
+              <td className="px-6 py-2">19/2/1973</td>
+              <td className="px-6 py-2">D.10</td>
+              <td className="px-6 py-2">0123456789</td>
+            </tr>
+            <tr className="bg-white dark:bg-gray-800 border-8" >
+              <td className="px-6 py-2">2</td>
+              <td className="px-6 py-2">John</td>
+              <td className="px-6 py-2">Nam</td>
+              <td className="px-6 py-2">19/2/1973</td>
+              <td className="px-6 py-2">D.10</td>
+              <td className="px-6 py-2">0123456789</td>
+            </tr>
+            <tr className="bg-white dark:bg-gray-800 border-8" >
+              <td className="px-6 py-2">3</td>
+              <td className="px-6 py-2">John</td>
+              <td className="px-6 py-2">Nam</td>
+              <td className="px-6 py-2">19/2/1973</td>
+              <td className="px-6 py-2">D.10</td>
+              <td className="px-6 py-2">0123456789</td>
+            </tr>
+            <tr className="bg-white dark:bg-gray-800 border-8" >
+              <td className="px-6 py-2">4</td>
+              <td className="px-6 py-2">John</td>
+              <td className="px-6 py-2">Nam</td>
+              <td className="px-6 py-2">19/2/1973</td>
+              <td className="px-6 py-2">D.10</td>
+              <td className="px-6 py-2">0123456789</td>
+            </tr>
+            <tr className="bg-white dark:bg-gray-800 border-8" >
+              <td className="px-6 py-2">5</td>
+              <td className="px-6 py-2">John</td>
+              <td className="px-6 py-2">Nam</td>
+              <td className="px-6 py-2">19/2/1973</td>
+              <td className="px-6 py-2">D.10</td>
+              <td className="px-6 py-2">0123456789</td>
+            </tr>
+            <tr className="bg-white dark:bg-gray-800 border-8" >
+              <td className="px-6 py-2">6</td>
+              <td className="px-6 py-2">John</td>
+              <td className="px-6 py-2">Nam</td>
+              <td className="px-6 py-2">19/2/1973</td>
+              <td className="px-6 py-2">D.10</td>
+              <td className="px-6 py-2">0123456789</td>
+            </tr>
+          </tbody>
+        </table>
+    },
+    {
+      label: "Phương tiện",
+      value: "vehicle",
+      children: 
+        <table className="w-full text-sm text-center text-gray-500 dark:text-gray-400">
+          <thead className=" text-gray-900 border-8 bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                ID
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Biển số
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Tải trọng
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Nhiên liệu
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Nơi sản xuất
+              </th>
+            </tr>
+          </thead>
+          <tbody className=" font-medium text-gray-900 whitespace-nowrap dark:text-white ">
+            <tr className="bg-white dark:bg-gray-800 border-8" >
+              <td className="px-6 py-2">1</td>
+              <td className="px-6 py-2">Hino FC9JETC</td>
+              <td className="px-6 py-2">5000</td>
+              <td className="px-6 py-2">50</td>
+              <td className="px-6 py-2">Nhật Bản</td>
+            </tr>
+            <tr className="bg-white dark:bg-gray-800 border-8" >
+              <td className="px-6 py-2">2</td>
+              <td className="px-6 py-2">Hino FC9JETC</td>
+              <td className="px-6 py-2">5000</td>
+              <td className="px-6 py-2">50</td>
+              <td className="px-6 py-2">Nhật Bản</td>
+            </tr>
+            <tr className="bg-white dark:bg-gray-800 border-8" >
+              <td className="px-6 py-2">3</td>
+              <td className="px-6 py-2">Hino FC9JETC</td>
+              <td className="px-6 py-2">5000</td>
+              <td className="px-6 py-2">50</td>
+              <td className="px-6 py-2">Nhật Bản</td>
+            </tr>
+            <tr className="bg-white dark:bg-gray-800 border-8" >
+              <td className="px-6 py-2">4</td>
+              <td className="px-6 py-2">Hino FC9JETC</td>
+              <td className="px-6 py-2">5000</td>
+              <td className="px-6 py-2">50</td>
+              <td className="px-6 py-2">Nhật Bản</td>
+            </tr>
+          </tbody>
+        </table>
+    },
+    {
+      label: "MCPs",
+      value: "MCPs",
+      children: 
+        <table className="w-full text-sm text-center text-gray-500 dark:text-gray-400">
+          <thead className=" text-gray-900 border-8 bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                ID
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Địa chỉ
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Sức Chứa
+              </th>
+            </tr>
+          </thead>
+          <tbody className=" font-medium text-gray-900 whitespace-nowrap dark:text-white ">
+            <tr className="bg-white dark:bg-gray-800 border-8" >
+              <td className="px-6 py-2">1</td>
+              <td className="px-6 py-2">Quận Bình Thạnh</td>
+              <td className="px-6 py-2">1000</td>
+            </tr>
+            <tr className="bg-white dark:bg-gray-800 border-8" >
+              <td className="px-6 py-2">2</td>
+              <td className="px-6 py-2">Quận 10</td>
+              <td className="px-6 py-2">1500</td>
+            </tr>
+            <tr className="bg-white dark:bg-gray-800 border-8" >
+              <td className="px-6 py-2">3</td>
+              <td className="px-6 py-2">Bến xe Q8</td>
+              <td className="px-6 py-2">2000</td>
+            </tr>
+            <tr className="bg-white dark:bg-gray-800 border-8" >
+              <td className="px-6 py-2">4</td>
+              <td className="px-6 py-2">Bến xe Miền Tây</td>
+              <td className="px-6 py-2">2500</td>
+            </tr>
+          </tbody>
+        </table>
     },
   ];
   return (
@@ -57,37 +301,6 @@ export const GeneralPaneComponent: IComponent = () => {
             ))}
           </TabsBody>
         </Tabs>
-      </div>
-
-      <div className="mcp">
-        MCP table here
-        {/* TODO: follows refs: https://tailwindcss.com/docs/table-layout */}
-        <table className="table-auto">
-          <thead>
-            <tr>
-              <th>Song</th>
-              <th>Artist</th>
-              <th>Year</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-              <td>Malcolm Lockyer</td>
-              <td>1961</td>
-            </tr>
-            <tr>
-              <td>Witchy Woman</td>
-              <td>The Eagles</td>
-              <td>1972</td>
-            </tr>
-            <tr>
-              <td>Shining Star</td>
-              <td>Earth, Wind, and Fire</td>
-              <td>1975</td>
-            </tr>
-          </tbody>
-        </table>
       </div>
     </div>
   );
