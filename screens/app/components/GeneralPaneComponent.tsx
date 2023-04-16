4a832500633856245f1e308432d3c53df3f38122
tranhuyducseven <tranhuyducseven@gmail.com>
2023-04-16 12:12:34 +0700

    hotfix/general: mockdata

--- screens/app/components/GeneralPaneComponent.tsx
+++ screens/app/components/GeneralPaneComponent.tsx
@@ -1,5 +1,4 @@
 import {
-  Button,
   Tab,
   TabPanel,
   Tabs,
@@ -7,215 +6,184 @@ import {
   TabsHeader,
 } from "@material-tailwind/react";
 
-//TODO: follow ref here: https://www.material-tailwind.com/docs/react/tabs
-
 export const GeneralPaneComponent: IComponent = () => {
-
   const data_janitor: {
-    ID: int;
     name: string;
     gender: string;
     day_of_birth: string;
     area: string;
-    phone: string
+    phone: string;
   }[] = [
     {
-      ID: "1",
       name: "John",
       gender: "Nam",
       day_of_birth: "19/2/1973",
       area: "D.10",
-      phone: "0123456789"
+      phone: "0123456789",
     },
     {
-      ID: "2",
       name: "John",
       gender: "Nam",
       day_of_birth: "19/2/1973",
       area: "D.10",
-      phone: "0123456789"
+      phone: "0123456789",
     },
     {
-      ID: "3",
       name: "John",
       gender: "Nam",
       day_of_birth: "19/2/1973",
       area: "D.10",
-      phone: "0123456789"
+      phone: "0123456789",
     },
     {
-      ID: "4",
       name: "John",
       gender: "Nam",
       day_of_birth: "19/2/1973",
       area: "D.10",
-      phone: "0123456789"
+      phone: "0123456789",
     },
     {
-      ID: "5",
       name: "John",
       gender: "Nam",
       day_of_birth: "19/2/1973",
       area: "D.10",
-      phone: "0123456789"
+      phone: "0123456789",
     },
     {
-      ID: "6",
       name: "John",
       gender: "Nam",
       day_of_birth: "19/2/1973",
       area: "D.10",
-      phone: "0123456789"
-    }
+      phone: "0123456789",
+    },
   ];
 
   const data_collector: {
-    ID: int;
     name: string;
     gender: string;
     day_of_birth: string;
     area: string;
-    phone: string
+    phone: string;
   }[] = [
     {
-      ID: "1",
       name: "John",
       gender: "Nam",
       day_of_birth: "19/2/1973",
       area: "D.10",
-      phone: "0123456789"
+      phone: "0123456789",
     },
     {
-      ID: "2",
       name: "John",
       gender: "Nam",
       day_of_birth: "19/2/1973",
       area: "D.10",
-      phone: "0123456789"
+      phone: "0123456789",
     },
     {
-      ID: "3",
       name: "John",
       gender: "Nam",
       day_of_birth: "19/2/1973",
       area: "D.10",
-      phone: "0123456789"
+      phone: "0123456789",
     },
     {
-      ID: "4",
       name: "John",
       gender: "Nam",
       day_of_birth: "19/2/1973",
       area: "D.10",
-      phone: "0123456789"
+      phone: "0123456789",
     },
     {
-      ID: "5",
       name: "John",
       gender: "Nam",
       day_of_birth: "19/2/1973",
       area: "D.10",
-      phone: "0123456789"
+      phone: "0123456789",
     },
     {
-      ID: "6",
       name: "John",
       gender: "Nam",
       day_of_birth: "19/2/1973",
       area: "D.10",
-      phone: "0123456789"
-    }
+      phone: "0123456789",
+    },
   ];
 
   const data_vehicle: {
-    ID: int;
     license_plates: string;
     capacity: string;
     fuel: string;
-    make_by: string
+    make_by: string;
   }[] = [
     {
-      ID: "1",
       license_plates: "Hino FC9JETC",
       capacity: "5000",
       fuel: "50",
-      make_by: "Nhật Bản"
+      make_by: "Nhật Bản",
     },
     {
-      ID: "2",
       license_plates: "Hino FC9JETC",
       capacity: "5000",
       fuel: "50",
-      make_by: "Nhật Bản"
+      make_by: "Nhật Bản",
     },
     {
-      ID: "3",
       license_plates: "Hino FC9JETC",
       capacity: "5000",
       fuel: "50",
-      make_by: "Nhật Bản"
+      make_by: "Nhật Bản",
     },
     {
-      ID: "4",
       license_plates: "Hino FC9JETC",
       capacity: "5000",
       fuel: "50",
-      make_by: "Nhật Bản"
+      make_by: "Nhật Bản",
     },
     {
-      ID: "5",
       license_plates: "Hino FC9JETC",
       capacity: "5000",
       fuel: "50",
-      make_by: "Nhật Bản"
+      make_by: "Nhật Bản",
     },
     {
-      ID: "6",
       license_plates: "Hino FC9JETC",
       capacity: "5000",
       fuel: "50",
-      make_by: "Nhật Bản"
-    }
+      make_by: "Nhật Bản",
+    },
   ];
 
   const data_MCPs: {
-    ID: int;
     location: string;
     MCPs_capacity: string;
   }[] = [
     {
-      ID: "1",
       location: "Quận Bình Thạnh",
       MCPs_capacity: "1000",
     },
     {
-      ID: "2",
       location: "Quận 10",
       MCPs_capacity: "1500",
     },
     {
-      ID: "3",
       location: "Quận Tân Bình",
       MCPs_capacity: "2000",
     },
     {
-      ID: "4",
       location: "Quận Bình Thạnh",
       MCPs_capacity: "2500",
     },
     {
-      ID: "5",
       location: "Quận 7",
       MCPs_capacity: "3000",
-    }
+    },
   ];
-
 
   const data = [
     {
       label: "Janitors",
       value: "janitors",
-      children: 
+      children: (
         <table className="w-full text-sm text-center text-gray-500 dark:text-gray-400">
           <thead className=" text-gray-900 border-8 bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
             <tr>
@@ -223,7 +191,7 @@ export const GeneralPaneComponent: IComponent = () => {
                 ID
               </th>
               <th scope="col" className="px-6 py-3">
-                Tên 
+                Tên
               </th>
               <th scope="col" className="px-6 py-3">
                 Giới tính
@@ -242,7 +210,7 @@ export const GeneralPaneComponent: IComponent = () => {
           <tbody className=" font-medium text-gray-900 whitespace-nowrap dark:text-white ">
             {data_janitor.map((item, index) => (
               <tr key={index} className="bg-white dark:bg-gray-800 border-8">
-                <td className="px-6 py-2">{item.ID}</td>
+                <td className="px-6 py-2">{index}</td>
                 <td className="px-6 py-2">{item.name}</td>
                 <td className="px-6 py-2">{item.gender}</td>
                 <td className="px-6 py-2">{item.day_of_birth}</td>
@@ -252,11 +220,12 @@ export const GeneralPaneComponent: IComponent = () => {
             ))}
           </tbody>
         </table>
+      ),
     },
     {
       label: "Collectors",
       value: "collectors",
-      children: 
+      children: (
         <table className="w-full text-sm text-center text-gray-500 dark:text-gray-400">
           <thead className=" text-gray-900 border-8 bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
             <tr>
@@ -264,7 +233,7 @@ export const GeneralPaneComponent: IComponent = () => {
                 ID
               </th>
               <th scope="col" className="px-6 py-3">
-                Tên 
+                Tên
               </th>
               <th scope="col" className="px-6 py-3">
                 Giới tính
@@ -283,7 +252,7 @@ export const GeneralPaneComponent: IComponent = () => {
           <tbody className=" font-medium text-gray-900 whitespace-nowrap dark:text-white ">
             {data_collector.map((item, index) => (
               <tr key={index} className="bg-white dark:bg-gray-800 border-8">
-                <td className="px-6 py-2">{item.ID}</td>
+                <td className="px-6 py-2">{index}</td>
                 <td className="px-6 py-2">{item.name}</td>
                 <td className="px-6 py-2">{item.gender}</td>
                 <td className="px-6 py-2">{item.day_of_birth}</td>
@@ -293,11 +262,12 @@ export const GeneralPaneComponent: IComponent = () => {
             ))}
           </tbody>
         </table>
+      ),
     },
     {
       label: "Phương tiện",
       value: "vehicle",
-      children: 
+      children: (
         <table className="w-full text-sm text-center text-gray-500 dark:text-gray-400">
           <thead className=" text-gray-900 border-8 bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
             <tr>
@@ -321,7 +291,7 @@ export const GeneralPaneComponent: IComponent = () => {
           <tbody className=" font-medium text-gray-900 whitespace-nowrap dark:text-white ">
             {data_vehicle.map((item, index) => (
               <tr key={index} className="bg-white dark:bg-gray-800 border-8">
-                <td className="px-6 py-2">{item.ID}</td>
+                <td className="px-6 py-2">{index}</td>
                 <td className="px-6 py-2">{item.license_plates}</td>
                 <td className="px-6 py-2">{item.capacity}</td>
                 <td className="px-6 py-2">{item.fuel}</td>
@@ -330,11 +300,12 @@ export const GeneralPaneComponent: IComponent = () => {
             ))}
           </tbody>
         </table>
+      ),
     },
     {
       label: "MCPs",
       value: "MCPs",
-      children: 
+      children: (
         <table className="w-full text-sm text-center text-gray-500 dark:text-gray-400">
           <thead className=" text-gray-900 border-8 bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
             <tr>
@@ -352,13 +323,14 @@ export const GeneralPaneComponent: IComponent = () => {
           <tbody className=" font-medium text-gray-900 whitespace-nowrap dark:text-white ">
             {data_MCPs.map((item, index) => (
               <tr key={index} className="bg-white dark:bg-gray-800 border-8">
-                <td className="px-6 py-2">{item.ID}</td>
+                <td className="px-6 py-2">{index}</td>
                 <td className="px-6 py-2">{item.location}</td>
                 <td className="px-6 py-2">{item.MCPs_capacity}</td>
               </tr>
             ))}
           </tbody>
         </table>
+      ),
     },
   ];
   return (
