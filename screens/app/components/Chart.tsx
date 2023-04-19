20d899fadc70daa253364fd36750dcf1224c352a
paradox2604 <66421827+paradox2604@users.noreply.github.com>
2023-04-19 12:56:22 +0700

    Update Chart.tsx

--- screens/app/components/Chart.tsx
+++ screens/app/components/Chart.tsx
@@ -1,43 +1,102 @@
-/* eslint-disable prettier/prettier */
-import React, { Component } from "react";
+import { Option, Select } from "@material-tailwind/react";
+import { capitalize } from "@utils/tools";
 import {
-  CartesianGrid,
-  Legend,
-  Line,
-  LineChart,
+  CategoryScale,
+  Chart as ChartJS,
+  LinearScale,
+  LineElement,
+  PointElement,
+  Title,
   Tooltip,
-  XAxis,
-  YAxis,
-} from "recharts";
+} from "chart.js";
+import { useEffect, useMemo, useState } from "react";
+import { Line } from "react-chartjs-2";
 
-class Chart extends Component {
-  render() {
-    const chartData = [
-      { time: "Jan", capacity: 30 },
-      { time: "Feb", capacity: 70 },
-      { time: "Mar", capacity: 80 },
-      { time: "Apr", capacity: 20 },
-      { time: "May", capacity: 10 },
-    ];
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  LineElement,
+  PointElement,
+  Title,
+  Tooltip
+);
 
-    return (
-      <LineChart width={1000} height={500} data={chartData}>
-        <XAxis dataKey="time" stroke="#374151" />
-        <YAxis dataKey="capacity" stroke="#374151" />
-        <CartesianGrid strokeDasharray="3 3" />
-        <Tooltip />
-        <Legend />
-        <Line
-          name="Line 1"
-          type="monotone"
-          dataKey="capacity"
-          stroke="#6B7280"
-          strokeWidth={2}
-          activeDot={{ r: 8 }}
-        />
-      </LineChart>
-    );
-  }
-}
+export const LineChart: IComponent<{
+  monitorType: string;
+}> = ({ monitorType = "temperature" }) => {
+  const [datasets, setDatasets] = useState<any>([]);
+  const options = {
+    scales: {
+      y: {
+        title: {
+          display: true,
+          text: capitalize(monitorType),
+          color: "black",
+          font: {
+            size: 20,
+          },
+        },
+        suggestedMax: 100,
+        suggestedMin: 0,
+        ticks: {
+          beginAtZero: true,
+          stepSize: 20,
+          color: "black",
+          font: {
+            size: 20,
+          },
+        },
+      },
+      x: {
+        title: {
+          display: true,
+          color: "black",
+          font: {
+            size: 16,
+          },
+        },
+        ticks: {
+          color: "black",
+          font: {
+            size: 16,
+          },
+        },
+      },
+    },
+  };
+  //TODO: fetch data here
+  useEffect(() => {
+    fetch("http://localhost:8000/api/data/list?page_id=2&page_size=100")
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+      })
+      .then((data) => {
+        const array = data.map((d: any) => d.value);
+        setDatasets(array);
+      });
+  }, []);
+  const chartData = useMemo(
+    () => ({
+      labels: datasets.map((_: any, index: number) => index),
+      datasets: [
+        {
+          label: "Khối lượng (tấn)",
+          data: datasets,
+          fill: false,
+          tension: 0.1,
+          borderColor: "#0E9CFF",
+          backgroundColor: "rgba(14, 156, 255, 0.2)", // Set background color here
+        },
+      ],
+    }),
+    [datasets]
+  );
 
-export default Chart;
+  return (
+    <div className="bg-white pt-8 pr-8">
+      <Line data={chartData} options={options} />;
+    </div>
+  );
+};
